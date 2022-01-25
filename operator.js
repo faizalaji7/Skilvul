9d90575bb05a5e3e21c6584392bbffac42d57ef9
faizalajiw <faizalajiwibowo7@gmail.com>
2022-01-26 01:44:53 +0700

    add file js

---
+++ operator.js
@@ -0,0 +1,56 @@
+// Penjumlahan
+let bilangan1 = 10;
+let bilangan2 = 3;
+
+console.log(bilangan1 + bilangan2); // Output: 13
+// Pengurangan
+let bilangan1 = 10;
+let bilangan2 = 3;
+
+console.log(bilangan1 - bilangan2); // Output: 7
+// Perkalian
+let bilangan1 = 10;
+let bilangan2 = 3;
+
+console.log(bilangan1 * bilangan2); // Output: 30
+// Pembagian
+let bilangan1 = 10;
+let bilangan2 = 3;
+
+console.log(bilangan1 / bilangan2); // Output: 3.3333333333333335
+// Eksponen (Pangkat)
+let bilangan1 = 10;
+let bilangan2 = 3;
+
+console.log(bilangan1 ** bilangan2); // Output: 1000
+// Modulus
+let bilangan1 = 10;
+let bilangan2 = 3;
+
+console.log(bilangan1 % bilangan2); // Output: 1
+// Increment (Tambah 1)
+let bilangan = 10;
+
+bilangan++;
+
+console.log(bilangan); // Output: 11
+// Contoh operasi di atas apabila dituliskan tanpa operator increment akan sama seperti ini:
+
+let bilangan = 10;
+
+bilangan = bilangan + 1;
+
+console.log(bilangan); // output: 11
+// Decrement (Kurang 1)
+let bilangan = 10;
+
+bilangan--;
+
+console.log(bilangan); // output: 9
+// Contoh operasi di atas apabila dituliskan tanpa operator decrement akan sama seperti ini:
+
+let bilangan = 10;
+
+bilangan = bilangan - 1;
+
+console.log(bilangan); // output: 9
