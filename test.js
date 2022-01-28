359db7bc53f80af124e6dde4e6f00d1880311d75
faizalajiw <faizalajiwibowo7@gmail.com>
2022-01-28 09:18:05 +0700

    update test js

--- test.js
+++ test.js
@@ -1,11 +1,9 @@
 // Bismillah Frontend Dev
 
-let array = ['Mangga', 'Pisang', 'Jambu', 1, 3, 5, 7];
-array.push("Ohaio"); // menambah element di bagian terakhir
-array.shift(); //mengeluarkan element pertama
-array.pop(); //mengeluarkan element terakhir dari sebuah array
-console.log(array.toString()); //menggabungkan dan mengubah element menjadi tipe data String
-console.log(array.join(",")); //menggabungkan element di array dengan menambah penghubung
+let fruits = ['Mangga', 'Pisang', 'Jambu'];
 
-console.log(array);
+console.log(fruits.splice(2, 0, 'Semangka')); 
+// console.log(fruits.push('Semangka')); 
+// console.log(fruits.slice(2, 0, 'Semangka'));
+console.log(fruits);
 // console.log(fruits.[length-1]);
