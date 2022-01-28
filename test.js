966b8ced4b1512b710223c0a1ece8365c0387ebc
faizalajiw <faizalajiwibowo7@gmail.com>
2022-01-28 08:27:33 +0700

    update js

---
+++ test.js
@@ -0,0 +1,7 @@
+// Bismillah Frontend Dev
+const promt = require('prompt-sync')();
+
+var name = promt("Nama Anda: ");
+var age = promt("Usia Anda: ");
+
+console.log("Saya ",name,", Usia saya ", age);
