260868bba8de8bc7dd492c247db447cce04dbed5
faizalajiw <faizalajiwibowo7@gmail.com>
2022-01-28 14:18:48 +0700

    update loop

---
+++ loop/do_while_loop.js
@@ -0,0 +1,17 @@
+let x = 1;
+
+do {
+  console.log(x);
+  x++;
+} while (x <= 10);
+
+// Contoh lain
+let angka = [];
+let i = 5;
+
+do {
+  angka.push(i);
+  i++;
+} while (i < 11);
+
+console.log(angka);
--- loop/for_loop.js
+++ loop/for_loop.js
@@ -1,4 +1,3 @@
-// Bismillah Frontend Dev
 let angka = [1, 46, 75, 12, 89, 54, 101];
 let genap = [];
 
