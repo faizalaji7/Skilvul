4f38c91fb03fedea331dc3523dc7a38b25c90168
faizalajiw <faizalajiwibowo7@gmail.com>
2022-03-25 23:47:34 +0700

    update materi .forEach

---
+++ Intermediate/01-Array-Iteration/.forEach()/forEach.js
@@ -0,0 +1,18 @@
+// ** Latihan 1
+// Lakukan perulangan pada array hewan menggunakan .forEach(). 
+// Beri callback function dengan parameter item sebagai value dari data array.
+
+// Di dalam callback function yang tadi dibuat, 
+// gunakan if statement sebagai conditional untuk mendapatkan list nama binatang dengan awalan huruf sebelum huruf "C" (contoh, "A" < "B").
+
+// Kemudian tampilkan item menggunakan console.log(). Klik tab Console untuk melihat list data.
+
+// ** Jawaban
+let hewan = ["Ayam", "Buaya", "Cicak", "Domba"];
+
+// Buat kode kamu di bawah ini
+hewan.forEach(item =>{
+    if(item[0] < "C"){
+        console.log(item)
+    } 
+});
